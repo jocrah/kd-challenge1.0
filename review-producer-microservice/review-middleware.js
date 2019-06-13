848c55df1bee8036b786ab52763e0580b7acc6bc
Josh <ocrahjoshua@gmail.com>
2019-06-13 20:55:49 +0000

    Added specific validation error messages

--- review-producer-microservice/review-middleware.js
+++ review-producer-microservice/review-middleware.js
@@ -1,4 +1,5 @@
 module.exports = function (options) {
+    let errorMessage = '';
     return function (req, res, next) {
 
         /*
@@ -18,10 +19,10 @@ module.exports = function (options) {
         let source = req.body.source;
         let type = req.body.type;
 
-        var errorMessage = '';
        
         if(!isValidBusinessID(businessID) || !isValidMessage(msg) || 
         !isValidRating(rating) || !isValidType(type) || !isValidSource(source)){
+            // return bad request
             return res.status(400).send({
                 "error": "Invalid parameters",
                 "message": errorMessage
@@ -32,63 +33,70 @@ module.exports = function (options) {
 
     function isValidBusinessID(id){
         if (typeof id == 'undefined') {
+            errorMessage = "business_id parameter is required"
             return false;
           }
           else if(typeof id !== 'number'){
-              return false;
+            errorMessage = "business_id parameter should be a number"
+            return false;
           }
-          console.log("business id passed");
           return true;
     }
 
     function isValidRating(rating){
         if (typeof rating == 'undefined') {
+            errorMessage = "rating parameter is required"
             return false;
           }
           else if(typeof rating !== 'number'){
+              errorMessage = "rating parameter must be a number"
               return false;
           }else if(rating < 0 || rating>5){
+              errorMessage = "rating parameter must fall between 1 and 5"
               return false;
           }
-          console.log("rating passed");
           return true;
     }
 
     function isValidType(type){
           if (typeof type == 'undefined') {
+            errorMessage = "type parameter is required"
             return false;
           }
           else if(typeof type !== 'string'){
-              console.log('1');
+            errorMessage = "type parameter must be a string"
               return false;
           }else if(type === 'product' || type === 'site'){
-              console.log("type passed");
               return true;
           }
+          errorMessage = "type should either be 'product' or 'site'"
           return false;
     }
 
     function isValidMessage(message){
         if (typeof message == 'undefined') {
+            errorMessage = "msg parameter is required"
             return false;
           }
           else if(typeof message !== 'string'){
-              return false;
+            errorMessage = "msg parameter is required"
+            return false;
           }
-          console.log("message passed");
           return true;
     }
 
     function isValidSource(source){
         if (typeof source == 'undefined') {
+            errorMessage = "source parameter is required"
             return false;
           }
           else if(typeof source !== 'string'){
+            errorMessage = "source parameter must be a string"
               return false;
-          }else if(source === 'amazon' || source === 'facebook'){
-              console.log("source passed");
+          }else if(source === 'amazon' || source === 'facebook' || source === 'kudobuzz'){
               return true;
           }
+          errorMessage = "source should either be 'amazon', 'facebook' or 'kudobuzz'"
           return false;
     }
   }
